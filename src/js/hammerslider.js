1c0a2b3371bcddcdbe539b5f295c1fedca783664
David Cetinkaya <david.o.cetinkaya@gmail.com>
2016-11-06 12:03:21 +0100

    Add todo in hammerslider.js

--- src/js/hammerslider.js
+++ src/js/hammerslider.js
@@ -6,6 +6,16 @@
  * MIT License
  */
 
+/*
+  - Publish to NPM
+  - Upload to github
+  - Correct all git links in: 
+      - Comments above
+      - package.json
+      - CodePen demo
+      - README.MD
+*/
+
 function HammerSlider(_this, options) {
   'use strict';
 
